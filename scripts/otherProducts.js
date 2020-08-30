f2800d4ca5c653570784680f1f2475bd40a0cab7
Oswaldo merino <lokuando@hotmail.com>
2020-08-30 01:27:16 -0500

    sala de cine

---
+++ scripts/otherProducts.js
@@ -0,0 +1,2 @@
+window.essentialJsUrl = "file:///C:/Users/Public/Documents/Syncfusion/JavaScript - EJ1/18.2.0.44/JavaScript/samples/web/default.htm";
+window.mobileJsUrl = "//js.syncfusion.com/demos/";
