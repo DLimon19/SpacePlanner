1f12be55fa5a5912b656ec6823674d8ccb982878
Roberto Díaz <robertocdiazsanchez@gmail.com>
2020-08-30 08:39:42 -0500

    Algoritmo de distribución de espacio
    Co-authored-by: DanielHdzSanchez

---
+++ algoritmo-distribucion/space-distribution-algorithm.js
@@ -0,0 +1,139 @@
+//Convierte un string como '2 3' a una tupla (2,3)
+function seat_to_point(seat)
+{
+    let tuple = seat.split(' ')
+    return tuple
+}
+
+function euclidean_distance(point1, point2)
+{
+    return Math.sqrt(Math.pow(Math.abs(point1[0]-point2[0]), 2) + Math.pow(Math.abs(point1[1]-point2[1]), 2))
+}
+
+function min_from_array(arr)
+{
+    let min_value = 9999
+    arr.forEach(val => {
+        if (val < min_value)
+            min_value = val
+    })
+
+    return min_value
+}
+
+let layout =[
+                [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+                [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+                [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+                [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+                [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+                [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+                [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+                [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+                [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+                [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+
+                [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+                [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+                [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+                [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+                [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+                [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+                [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+                [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+                [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+                [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1]
+            ]
+let total=0;
+// layout.forEach((num)=> {
+//     total+=num
+// });
+layout.forEach(row => row.forEach(val => total += val))
+
+//Porcentaje admitido
+let percentage = .19
+//Distancia en metros
+let seat_side_length = .75
+let safe_distance = 1.5
+let safe_units = Math.ceil(safe_distance / seat_side_length)
+
+console.log('total: ' + total)
+console.log('percentage: ' + percentage)
+console.log('seat_side_length: ' + seat_side_length)
+console.log('safe_distance: ' + safe_distance)
+console.log('safe_units: ' + safe_units)
+
+let k = Math.floor(total * percentage)
+
+let corners = [
+    '0 0',
+    (layout[0].length - 1) + ' 0',
+    '0 ' + String(layout.length - 1),
+    String((layout[0].length - 1)) + ' ' + String(layout.length - 1)
+]
+
+let is_possible = true;
+while (corners.length < k){
+    new_corner = ''
+    min_mean = 9999
+
+    for (y = 0; y < layout.length; ++y) 
+    {
+        for (x = 0; x < layout[0].length; ++x) 
+        {
+            if (!((String(x) + ' ' + String(y)) in corners) && layout[y][x] >=1)
+            {
+                let distances = []
+                corners.forEach((c)=>
+                {
+                    distances.push(euclidean_distance([x,y], seat_to_point(c)))
+                })
+
+                let mean = 0;
+                //distances = distances.slice();
+                let dlength = distances.length;
+                let a = 0;
+                distances.forEach((n)=>
+                {
+                    a+=n;
+                })
+                mean = a/dlength;
+                if((mean < min_mean) && (min_from_array(distances) > safe_units))
+                {
+                    min_mean = mean;
+                    new_corner = String(x) + ' ' + String(y);
+                }
+            }
+        }
+    }
+    if (new_corner=='')
+    {
+        console.log('Not possible!')
+        is_possible = false;
+    }
+
+    if (!is_possible)
+        break;
+
+    corners.push(new_corner)
+}
+
+if (is_possible) {
+    corners.forEach(corner => {
+        [x, y] = seat_to_point(corner)
+        layout[y][x] = 2
+    })
+    
+    // console.log(layout)
+    for (let y = 0; y < layout.length; y++) {
+        let row = ''
+        for (let x = 0; x < layout[0].length; x++) {
+            row += `${layout[y][x]} `
+        }
+
+        console.log(row);
+    }
+
+    // layout.forEach(row => row.forEach())
+}
+    
---
+++ algoritmo-distribucion/space-distribution-algorithm.py
@@ -0,0 +1,121 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+def seat_to_point(seat):
+    """ Convierte un string como '2 3' a una tupla (2, 3) """
+    return list(map(lambda x: int(x), seat.split(" ")))
+
+def euclidean_distance(point1, point2):
+    return math.sqrt( (abs(point1[0] - point2[0]) ** 2) + (abs(point1[1] - point2[1]) ** 2) )
+
+# layout = np.array([
+#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+# ])
+
+# layout = np.array([
+#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+# ])
+
+layout = np.array([
+    [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+
+    [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+])
+
+
+total = layout.sum()
+percentage = .19
+seat_side_length = .75
+safe_distance = 1.5
+safe_units = math.ceil(safe_distance / seat_side_length)
+
+print(f"total = {total}")
+print(f"percentage = {percentage}")
+print(f"seat_side_length = {seat_side_length}")
+print(f"safe_distance = {safe_distance}")
+print(f"safe_units = {safe_units}")
+
+k = math.floor(total * percentage)
+
+corners = [
+    f"0 0", 
+    f"{len(layout[0]) - 1} 0", 
+    f"0 {len(layout) - 1}", 
+    f"{len(layout[0]) - 1} {len(layout) - 1}"
+]
+
+while len(corners) < k:
+    new_corner = ""
+    min_mean = 9999
+    
+    for y in range(len(layout)):
+        for x in range(len(layout[0])):
+            if f"{x} {y}" not in corners and layout[y][x] >= 1:
+
+                distances = []
+                for c in corners:
+                    distances.append(euclidean_distance((x, y), seat_to_point(c)))
+                
+                distances = np.copy(distances)
+                if (distances.mean() < min_mean) and (distances.min() > safe_units):
+                    min_mean = distances.mean()
+                    new_corner = f"{x} {y}"
+
+
+    if new_corner == "":
+        print("Not possible!")
+        exit()
+
+    corners.append(new_corner)
+
+for corner in corners:
+    x, y = seat_to_point(corner)
+    layout[y][x] = 2
+
+for y in range(len(layout)):
+    for x in range(len(layout[0])):
+        if layout[y][x] >= 1:
+            plt.scatter(x, y, c = "green" if layout[y][x] == 2 else "lightgray")
+
+plt.show()
+
+
+
+
